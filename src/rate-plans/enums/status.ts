7b24fe562f335b22e521cce97b6746895bbfe7b8
Andrés Weitzel <andres96energy@hotmail.com>
2023-08-06 21:08:10 -0300

    Updated model and enum for rate-plans

---
+++ src/rate-plans/enums/status.ts
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+/**
+ * @description enum class. This enum returns only published rate plans with the Published status value. Published rate plans are approved and available for use by devices.
+ */
+export enum Status{
+    PUBLISHED= 'Published',
+    ACTIVE= 'Active',
+    INACTIVE='Inactive'
+}
--- src/rate-plans/models/rate-plants.entity.ts
+++ src/rate-plans/models/rate-plants.entity.ts
@@ -1,4 +1,6 @@
 /* eslint-disable prettier/prettier */
+//External
+import { ObjectType } from '@nestjs/graphql';
 import { ApiProperty, ApiTags } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 import {
@@ -9,7 +11,12 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+//Enums
+import { Status } from '../enums/status';
 
+@ObjectType()
+@Entity({ name: 'rate_plans' })
+@ApiTags('RatePlans')
 export class RatePlans extends BaseEntity {
   /**
    * @description Identifier for a rate plan
@@ -23,7 +30,7 @@ export class RatePlans extends BaseEntity {
     minimum: 1,
     maximum: 999999999,
     nullable: false,
-    example: '123',
+    example: 123,
   })
   id: number;
 
@@ -62,7 +69,7 @@ export class RatePlans extends BaseEntity {
     description:
       'Uniquely identifies a particular version of a rate plan in relation to every other rate plan version in the system, including different versions of the same rate plan and different versions of other rate plans. Because a rate plan may have multiple active versions at any one time, the versionId, not the id, is the only truly unique rate plan identifier.',
     type: 'int',
-    example: '4346666555',
+    example: 4346666555,
   })
   versionId: number;
 
@@ -85,4 +92,115 @@ export class RatePlans extends BaseEntity {
     example: '1.1',
   })
   versionPlan: string;
+
+  /**
+   * @description enum propertie. This enum returns only published rate plans with the Published status value. Published rate plans are approved and available for use by devices.
+   */
+  @Column({
+    name: 'status',
+    type: 'enum',
+    enum: Status,
+    default: Status.ACTIVE,
+    nullable: false,
+  })
+  @IsNotEmpty()
+  @ApiProperty({
+    name: 'status',
+    description: 'status for a product',
+    type: 'enum',
+    example: 'ACTIVE (only enum: ACTIVE, INACTIVE or PUBLISHED)',
+  })
+  status: Status;
+
+  /**
+   * @description The type refers to the payment method (monthly vs. prepaid) and included usage type (individual vs. pooled vs. event).
+   */
+  @Column({
+    name: 'type_plan',
+    type: 'string',
+    nullable: false,
+  })
+  @IsNotEmpty()
+  @ApiProperty({
+    name: 'type_plan',
+    description:
+      'The type refers to the payment method (monthly vs. prepaid) and included usage type (individual vs. pooled vs. event).',
+    type: 'string',
+    minLength: 2,
+    maxLength: 200,
+    example: 'plan for individual annual subscriptions',
+  })
+  typePlan: string;
+
+  /**
+   * @description	For monthly rate plans, this charge is the monthly subscription fee for each of the subscribers in the tier. For prepaid rate plans, this is a one-time charge per subscriber that covers the entire term of the plan.
+   */
+  @Column({
+    name: 'subscription_charge',
+    type: 'decimal',
+    nullable: true,
+    default: 2.99,
+    precision: 6,
+    scale: 3,
+  })
+  @ApiProperty({
+    name: 'subscription_charge',
+    description:
+      'For monthly rate plans, this charge is the monthly subscription fee for each of the subscribers in the tier. For prepaid rate plans, this is a one-time charge per subscriber that covers the entire term of the plan.',
+    type: 'decimal',
+    example: 2.99,
+  })
+  subscriptionCharge: number;
+
+  /**
+   * @description		The number of tiers specifies how many subscription tiers the rate plan has. Each tier can have different subscription fees, account charges (for certain plan types), usage allotments, and overage charges..
+   */
+  @Column({
+    name: 'number_of_tiers',
+    type: 'int',
+    nullable: true,
+    default: 1,
+  })
+  @ApiProperty({
+    name: 'number_of_tiers',
+    description:
+      '	The number of tiers specifies how many subscription tiers the rate plan has. Each tier can have different subscription fees, account charges (for certain plan types), usage allotments, and overage charges.',
+    type: 'int',
+    example: 1,
+  })
+  number_of_tiers: number;
+
+  /**
+   * @description	creation date for a rate-plan record
+   */
+  @CreateDateColumn({
+    name: 'creation_date',
+    type: 'datetime',
+    nullable: false,
+  })
+  @IsNotEmpty()
+  @ApiProperty({
+    name: 'creationDate',
+    description: 'creation date for a rate-plan record',
+    type: 'string',
+    minLength: 4,
+    maxLength: 50,
+    example: '2023-02-17T00:47:58.000Z',
+  })
+  creationDate: Date;
+
+  /**
+   * @description	update date for a rate-plan record
+   */
+  @UpdateDateColumn({ name: 'update_date', type: 'datetime', nullable: false })
+  @IsNotEmpty()
+  @ApiProperty({
+    name: 'updateDate',
+    description: 'update date for a product',
+    type: 'string',
+    minLength: 4,
+    maxLength: 50,
+    example: '2023-02-17T00:47:58.000Z',
+  })
+  updateDate: Date;
 }
