55e6eea1cb744f665cb41d10cab652592f0bf058
Andrés Weitzel <andres96energy@hotmail.com>
2023-09-07 19:55:33 -0300

    Added table and records

--- src/rate-plans/models/rate-plans.entity.ts
+++ src/rate-plans/models/rate-plans.entity.ts
@@ -12,6 +12,24 @@ import {
 } from 'typeorm';
 //Enums
 import { Status } from '../enums/status';
+//Const-vars
+const MIN_VALUE_ID = 1;
+const MAX_VALUE_ID = 9007199254740991n;
+const DECIMAL_SCALE_VALUE = 3;
+const DECIMAL_PRECISION_VALUE = 6;
+const MIN_VALUE_FOR_NAME = 4;
+const MAX_VALUE_FOR_NAME = 200;
+const MIN_VALUE_FOR_DESCRIPTION = 4;
+const MAX_VALUE_FOR_DESCRIPTION = 500;
+const MIN_VALUE_FOR_VERSION_PLAN = 1;
+const MAX_VALUE_FOR_VERSION_PLAN = 20;
+const MIN_VALUE_FOR_TYPE_PLAN = 2;
+const MAX_VALUE_FOR_TYPE_PLAN = 200;
+const DEFAULT_VALUE_FOR_SUSCRIPTION_CHARGE = 2.99;
+const MIN_VALUE_FOR_NUMBER_OF_TIERS = 1;
+const MIN_VALUE_LENGTH_DATE = 4;
+const MAX_VALUE_LENGTH_DATE = 50;
+
 
 @ObjectType('RatePlans')
 @Entity({ name: 'rate_plans' })
@@ -21,13 +39,13 @@ export class RatePlans extends BaseEntity {
    * @description Identifier for a rate plan
    */
   @Field()
-  @PrimaryGeneratedColumn('increment', { type: 'int' })
+  @PrimaryGeneratedColumn('increment', { type: 'bigint' })
   @ApiProperty({
     name: 'id',
     description: 'Identifier for a rate plan',
-    type: 'int',
-    minimum: 1,
-    maximum: 999999999,
+    type: 'bigint',
+    minimum: MIN_VALUE_ID,
+    maximum: Number(MAX_VALUE_ID),
     nullable: false,
     example: 123,
   })
@@ -39,7 +57,7 @@ export class RatePlans extends BaseEntity {
   @Field()
   @Column({
     name: 'name',
-    length: 200,
+    length: MAX_VALUE_FOR_NAME,
     nullable: false,
   })
   @ApiProperty({
@@ -47,8 +65,8 @@ export class RatePlans extends BaseEntity {
     description:
       'Descriptive rate plan name. Names are unique within an operator.',
     type: 'string',
-    minLength: 4,
-    maxLength: 200,
+    minLength: MIN_VALUE_FOR_NAME,
+    maxLength: MAX_VALUE_FOR_NAME,
     example: 'plan for individual monthly subscriptions',
   })
   name: string;
@@ -59,15 +77,15 @@ export class RatePlans extends BaseEntity {
   @Field()
   @Column({
     name: 'description',
-    length: 500,
+    length: MAX_VALUE_FOR_DESCRIPTION,
     nullable: false,
   })
   @ApiProperty({
     name: 'description',
     description: 'Description for a plan.',
     type: 'string',
-    minLength: 4,
-    maxLength: 500,
+    minLength: MIN_VALUE_FOR_DESCRIPTION,
+    maxLength: MAX_VALUE_FOR_DESCRIPTION,
     example: 'plan for individual monthly subscriptions',
   })
   description: string;
@@ -96,7 +114,7 @@ export class RatePlans extends BaseEntity {
   @Field()
   @Column({
     name: 'version_plan',
-    length: 20,
+    length: MAX_VALUE_FOR_VERSION_PLAN,
     nullable: true,
     default: '1.0',
   })
@@ -105,8 +123,8 @@ export class RatePlans extends BaseEntity {
     description:
       'Specifies the version of the current rate plan. Two different versions of the same rate plan may coexist if there are billable devices assigned to each version (for prepaid plans only)',
     type: 'string',
-    minLength: 1,
-    maxLength: 20,
+    minLength: MIN_VALUE_FOR_VERSION_PLAN,
+    maxLength: MAX_VALUE_FOR_VERSION_PLAN,
     example: '1.1',
   })
   versionPlan: string;
@@ -136,7 +154,7 @@ export class RatePlans extends BaseEntity {
   @Field()
   @Column({
     name: 'type_plan',
-    length: 200,
+    length: MAX_VALUE_FOR_TYPE_PLAN,
     nullable: false,
   })
   @ApiProperty({
@@ -144,8 +162,8 @@ export class RatePlans extends BaseEntity {
     description:
       'The type refers to the payment method (monthly vs. prepaid) and included usage type (individual vs. pooled vs. event).',
     type: 'string',
-    minLength: 2,
-    maxLength: 200,
+    minLength: MIN_VALUE_FOR_TYPE_PLAN,
+    maxLength: MAX_VALUE_FOR_TYPE_PLAN,
     example: 'plan for individual annual subscriptions',
   })
   typePlan: string;
@@ -158,16 +176,16 @@ export class RatePlans extends BaseEntity {
     name: 'subscription_charge',
     type: 'decimal',
     nullable: true,
-    default: 2.99,
-    precision: 6,
-    scale: 3,
+    default: DEFAULT_VALUE_FOR_SUSCRIPTION_CHARGE,
+    precision: DECIMAL_PRECISION_VALUE,
+    scale: DECIMAL_SCALE_VALUE,
   })
   @ApiProperty({
     name: 'subscriptionCharge',
     description:
       'For monthly rate plans, this charge is the monthly subscription fee for each of the subscribers in the tier. For prepaid rate plans, this is a one-time charge per subscriber that covers the entire term of the plan.',
     type: 'decimal',
-    example: 2.99,
+    example: DEFAULT_VALUE_FOR_SUSCRIPTION_CHARGE,
   })
   subscriptionCharge: number;
 
@@ -179,14 +197,14 @@ export class RatePlans extends BaseEntity {
     name: 'number_of_tiers',
     type: 'int',
     nullable: true,
-    default: 1,
+    default: MIN_VALUE_FOR_NUMBER_OF_TIERS,
   })
   @ApiProperty({
     name: 'numberOfTiers',
     description:
       '	The number of tiers specifies how many subscription tiers the rate plan has. Each tier can have different subscription fees, account charges (for certain plan types), usage allotments, and overage charges.',
     type: 'int',
-    example: 1,
+    example: MIN_VALUE_FOR_NUMBER_OF_TIERS,
   })
   numberOfTiers: number;
 
@@ -203,8 +221,8 @@ export class RatePlans extends BaseEntity {
     name: 'creationDate',
     description: 'creation date for a rate-plan record',
     type: 'datetime',
-    minLength: 4,
-    maxLength: 50,
+    minLength: MIN_VALUE_LENGTH_DATE,
+    maxLength: MAX_VALUE_LENGTH_DATE,
     example: '2023-02-17T00:47:58.000Z',
   })
   creationDate: Date;
@@ -218,8 +236,8 @@ export class RatePlans extends BaseEntity {
     name: 'updateDate',
     description: 'update date for a product',
     type: 'datetime',
-    minLength: 4,
-    maxLength: 50,
+    minLength: MIN_VALUE_LENGTH_DATE,
+    maxLength: MAX_VALUE_LENGTH_DATE,
     example: '2023-02-17T00:47:58.000Z',
   })
   updateDate: Date;
